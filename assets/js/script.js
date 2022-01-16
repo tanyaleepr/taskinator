e24574a5a9aee722a45689d675f0b92adcbcbc4e
tanyaleepr <tanyaleepr@gmail.com>
2022-01-15 20:01:07 -0600

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
@@ -281,16 +280,3 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
 loadTasks();
-=======
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-};
-
-buttonEl.addEventListener("click", createTaskHandler);
->>>>>>> main
